cf4b504a1bbee8e251a452a4c91ef31c309a99e3
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-12-21 01:06:19 +0100

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:8]
-print("First 3 letters: {:s}".format(word_first_3))
-print("Last 2 letters: {:s}".format(word_last_2))
-print("Middle word: {:s}".format(middle_word))
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
