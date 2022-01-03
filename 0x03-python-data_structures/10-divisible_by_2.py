6a4e00801754af6ce0d32f07eb5aedbca92daec5
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-03 19:38:15 +0100

    /2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return my_list
-    new_list = list(my_list)
-    for index in new_list:
-        if index % 2 == 0:
-            new_list[index] = True
-        else:
-            new_list[index] = False
-    return new_list
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
