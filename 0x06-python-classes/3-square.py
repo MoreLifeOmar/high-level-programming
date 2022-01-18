e5f58ae1919b2d7c28d53795fca5d2aaa1541813
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-18 20:44:49 +0100

    Public instance

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Private Attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+        """Initialization"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        self.area
+        """Public instance"""
