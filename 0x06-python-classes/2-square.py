95cdb9ab75c30a72cd029f94247ca0bf8d9b61b5
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-18 17:28:39 +0100

    exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""Module Square"""
+
+
 class Square:
+    """Private Attribute"""
     def __init__(self, size=0):
-        
+        self.__size = size
+        """TypeError Exception"""
+        try:
+            size(int(input))
+        except TypeError:
+            raise Exception("size must be an integer")
+        """ValueError Exception"""
+        try:
+            size >= 0
+        except ValueError:
+            raise Exception("size must be >= 0")
