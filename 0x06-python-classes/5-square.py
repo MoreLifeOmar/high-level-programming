5b5a4ba558c0d0828718c61d8032fddbc9c08b8e
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-19 13:04:01 +0100

    square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Private Attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+        """Initialization"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+            set square area
+            Return:
+            square area (int)
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter of __size
+
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+
+        Args:
+            value (int): the size of a size of the square
+
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        """prints the square
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
