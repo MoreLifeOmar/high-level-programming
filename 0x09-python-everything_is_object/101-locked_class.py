189e0a59b3e43f0cf8c6051d1f4c17f9cac5b2ac
Omar Cherni <89669057+omarcherni007@users.noreply.github.com>
2022-01-25 18:25:35 +0100

    Fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-class LockedClass(object):
-    __slots__ = 'first_name'
+"""
+LockedClass
+"""
+
+
+class LockedClass:
+    """ No class or object attributes, can't set
+        Except for first_name
+    """
+    def __setattr__(self, attribute, value):
+        if attribute == "first_name":
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
