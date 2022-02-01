0817d0fb289619d8174011d2a8a26eea0909613e
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-01 19:04:12 +0100

    Documentation Fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the read_file function
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
