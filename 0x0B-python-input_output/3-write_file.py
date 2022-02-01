afa7f352af737066b09f884879c2ac135afcca28
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-01 19:06:16 +0100

    Documentation Fixed

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the function "wrtie_file"
+"""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
