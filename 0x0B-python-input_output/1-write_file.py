a9679007f0f16cca31581594d8d82bb4dbe0f703
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-01 19:17:19 +0100

    Fixing Documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_lines function
-"""
+"""write file."""
 
 
-def number_of_lines(filename=""):
-    """returns the number of lines of a text file"""
-    with open(filename, 'r', encoding='utf8') as f:
-        i = 0
-        for line in f:
-            i += 1
-        return i
+def write_file(filename="", text=""):
+    """write file."""
+    with open(filename, mode='w', encoding='UTF8') as f:
+        chars = f.write(text)
+    return chars
