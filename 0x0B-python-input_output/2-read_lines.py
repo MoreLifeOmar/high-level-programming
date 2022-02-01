2146a240b1586a8a4fa9c817723ed8e3162ecab9
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-01 19:05:20 +0100

    Documentation Fixed

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains the "read_lines" function
+"""
 
 
 def read_lines(filename="", nb_lines=0):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """reads n lines of a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
         if nb_lines <= 0:
-            print(f.read(), end="")
-        for index in range(nb_lines):
-            print(f.readline(), end="")
+            print(f.read(), end='')
+            return
+        i = 0
+        for i, line in enumerate(f):
+            if i == nb_lines:
+                break
+            print(line, end='')
