3c7bf524cb5866aa2d522dfd68084446fea2c6d7
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-01 19:06:48 +0100

    Documentation Fixed

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the function "append_wrtie"
+"""
 
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
+    """returns the number of chars appended to "filename" from "text" """
+    with open(filename, 'a', encoding='utf=8') as f:
         return f.write(text)
