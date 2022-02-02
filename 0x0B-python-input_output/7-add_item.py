fd809d6924198f615f59ee50bee2ddde1ab75e42
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-02 20:57:15 +0100

    Finally

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list."""
-from sys import argv
-from os import stat, path
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""
+adds argv to a Python list, and save them to a file
+"""
+import sys
 
+_save = __import__('5-save_to_json_file').save_to_json_file
+_load = __import__('6-load_from_json_file').load_from_json_file
 
-def main():
-    """main module"""
-    file = "add_item.json"
-    if path.isfile(file) and stat(file).st_size != 0:
-        av = load_from_json_file(file)
-    else:
-        av = []
-    av.extend(argv[1:])
-    save_to_json_file(av, file)
-
-
-if __name__ == "__main__":
-    main()
+open("add_item.json", "a")
+try:
+    x = _load("add_item.json")
+except ValueError:
+    x = []
+_save(x + sys.argv[1:], "add_item.json")
