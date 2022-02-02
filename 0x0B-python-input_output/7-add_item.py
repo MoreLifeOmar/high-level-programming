3ad84adb0ac29e79c45f472d7a4469a37489304e
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-02-02 20:43:49 +0100

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
-
+"""Adds all arguments to a Python list."""
 from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+from os import stat, path
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-filename = "add_item.json"
 
-try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
+def main():
+    """main module"""
+    file = "add_item.json"
+    if path.isfile(file) and stat(file).st_size != 0:
+        av = load_from_json_file(file)
+    else:
+        av = []
+    av.extend(argv[1:])
+    save_to_json_file(av, file)
 
-for arg in argv[1:]:
-    json_list.append(arg)
 
-save_to_json_file(json_list, filename)
+if __name__ == "__main__":
+    main()
