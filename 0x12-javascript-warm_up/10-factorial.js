57383f11b5a968ca58a60a61e3b6286a6b0ee31d
ocherni <89669057+ocherni@users.noreply.github.com>
2022-08-01 17:33:47 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-console.log(factorial(parseInt(process.argv[2])));
-
-function factorial (a) {
-  if (isNaN(a) || a === 1) { return 1; }
-  return a * (factorial(a - 1));
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-myArray.forEach((value, index) => {
-  console.log(value);
-});
