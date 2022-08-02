15ca64c951725b338c4740cff6a15fe81d41eea9
ocherni <89669057+ocherni@users.noreply.github.com>
2022-08-02 20:56:10 +0200

    Create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w && h && w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const w = this.width;
+    this.width = this.height;
+    this.height = w;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
