b907b2da72ebd509251a8877ffd153b2acd217e3
ocherni <89669057+ocherni@users.noreply.github.com>
2022-08-19 00:14:48 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98"
+#Bash script that takes in a URL as an argument, sends a GET
+curl -s "$1" GET -H "X-School-User-Id: 98"
