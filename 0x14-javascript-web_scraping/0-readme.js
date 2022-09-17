0a317483277e30dd3e2cda0e52b3513555c4a0ca
ocherni <89669057+ocherni@users.noreply.github.com>
2022-09-17 18:27:48 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', function (err, data) {
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
